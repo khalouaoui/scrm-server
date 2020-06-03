37275de1a5194a8a9639423c502b8117f2677f01
khalouaoui <khalouaoui_ibrahim@hotmail.fr>
2020-06-03 23:54:16 +0100

    sprint1

---
+++ .history/config/db_20200603234204.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://ibrahim:P28exK2CYRO6O1iN@cluster0-teusw.mongodb.net/test?retryWrites=true&w=majority',
+    secret: 'cccdzdazzegehe'
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# crm
+gestion de réseaux sociaux 
